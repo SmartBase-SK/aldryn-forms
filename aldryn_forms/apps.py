db16ac1b7faefb65560174e6a10070efcd267857
Unknown <epribula@gmail.com>
2018-09-06 13:24:11 +0200

    Added app config

---
+++ aldryn_forms/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class FormsConfig(AppConfig):
+    label = "aldryn_forms"
+    name = "aldryn_forms"
+    verbose_name = _("Forms")
