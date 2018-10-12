5be6981d9bf111bf3cc94797649c8201057062f5
Unknown <epribula@gmail.com>
2018-10-12 11:47:15 +0200

    Fixed bugs related to text field counter, improved behavior

--- aldryn_forms/static/form-helper.js
+++ aldryn_forms/static/form-helper.js
@@ -1,27 +1,37 @@
 $(document).ready(function () {
-    $('.textfield').keydown(function (e) {
-        let text_length = $(e.target).prop('value').length;
-        let min_length = parseInt($(e.target).attr('minlength'));
-        let max_length = parseInt($(e.target).attr('maxlength'));
-        $(e.target).next('span').text(text_length);
-        if (text_length < min_length || text_length > max_length)
-            $(e.target).next('span').addClass('counter__red');
-        else
-            $(e.target).next('span').removeClass('counter__red');
-    });
-
-    if ($('.dateinput').length > 0) {
-        $('.dateinput').datepicker({
-            changeMonth: true,
-            changeYear: true,
-            yearRange: "1850:2050",
-            dateFormat: 'dd.mm.yy',
-            beforeShow: function (el, dp) {
-                $(el).parent().append($('#ui-datepicker-div'));
-                $('#ui-datepicker-div').hide();
-            },
-        });
-
+    let $text_inputs = $('.textfield');
+    if ($text_inputs.length) {
+        $.each($text_inputs, function (index, e) {
+            $(e).on('input', count_input);
+        })
     }
 
+    $('.dateinput').datepicker({
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "1850:2050",
+        dateFormat: 'dd.mm.yy',
+        beforeShow: function (el, dp) {
+            $(el).parent().append($('#ui-datepicker-div'));
+            $('#ui-datepicker-div').hide();
+        },
+    });
 });
+
+function count_input(e) {
+    let elm = e.target ? e.target : e;
+    let text = $(elm).prop('value');
+    let text_length = text ? text.length : 0;
+    let min_length = parseInt($(elm).attr('minlength'));
+    let max_length = parseInt($(elm).attr('maxlength'));
+
+    if (text && max_length && max_length < text_length)
+        $(elm).prop('value', text.substring(0, max_length));
+
+    $(elm).next('span').text(text_length);
+    if ((min_length && text_length < min_length) || (max_length && text_length > max_length))
+        $(elm).next('span').addClass('counter__red');
+    else
+        $(elm).next('span').removeClass('counter__red');
+}
+
