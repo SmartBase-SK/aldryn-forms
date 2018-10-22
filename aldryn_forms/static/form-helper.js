aec18b83fa8c67094861a5714af51514d33c8e3b
Robert Filo <robert.filo@smartbase.sk>
2018-10-22 10:36:14 +0200

    triv: dateinput check if exists, show counter only if min and max length is set

--- aldryn_forms/static/form-helper.js
+++ aldryn_forms/static/form-helper.js
@@ -1,21 +1,17 @@
 $(document).ready(function () {
-    let $text_inputs = $('.textfield');
-    if ($text_inputs.length) {
-        $.each($text_inputs, function (index, e) {
-            $(e).on('input', count_input);
-        })
+    $('.textfield').on('input', count_input);
+    if ($('.dateinput').length > 0) {
+        $('.dateinput').datepicker({
+            changeMonth: true,
+            changeYear: true,
+            yearRange: "1850:2050",
+            dateFormat: 'dd.mm.yy',
+            beforeShow: function (el, dp) {
+                $(el).parent().append($('#ui-datepicker-div'));
+                $('#ui-datepicker-div').hide();
+            },
+        });
     }
-
-    $('.dateinput').datepicker({
-        changeMonth: true,
-        changeYear: true,
-        yearRange: "1850:2050",
-        dateFormat: 'dd.mm.yy',
-        beforeShow: function (el, dp) {
-            $(el).parent().append($('#ui-datepicker-div'));
-            $('#ui-datepicker-div').hide();
-        },
-    });
 });
 
 function count_input(e) {
@@ -25,13 +21,17 @@ function count_input(e) {
     let min_length = parseInt($(elm).attr('minlength'));
     let max_length = parseInt($(elm).attr('maxlength'));
 
-    if (text && max_length && max_length < text_length)
-        $(elm).prop('value', text.substring(0, max_length));
+    if (min_length || max_length){
+        if (text_length == 0){
+            $(elm).next('span').text("");
+        } else {
+            $(elm).next('span').text(text_length);
+        }
 
-    $(elm).next('span').text(text_length);
-    if ((min_length && text_length < min_length) || (max_length && text_length > max_length))
-        $(elm).next('span').addClass('counter__red');
-    else
-        $(elm).next('span').removeClass('counter__red');
+        if ((min_length && text_length < min_length) || (max_length && text_length > max_length))
+            $(elm).next('span').addClass('counter__red');
+        else
+            $(elm).next('span').removeClass('counter__red');
+    }
 }
 
